b72bce0f692d76e599d53b0bc0c5de55ac766d76
bibek <bibekgaihre123@outlook.com>
2020-06-18 20:24:57 +0545

    Admin UI's added

--- modules/admin/admin.routes.ui.js
+++ modules/admin/admin.routes.ui.js
@@ -6,4 +6,10 @@ router.get("/login", function (req, res, next) {
 router.get("/event/add", function (req, res, next) {
   res.render("admin/addevent");
 });
+router.get("/event/list", function (req, res, next) {
+  res.render("admin/listevent");
+});
+router.get("/content/update", function (req, res, next) {
+  res.render("admin/editcms");
+});
 module.exports = router;
--- modules/general/general.routes.api.js
+++ modules/general/general.routes.api.js
@@ -1,43 +1,43 @@
-const { NotExtended } = require("http-errors");
+// const { NotExtended } = require("http-errors");
 
-const router = require("express").Router();
-const mailer = require("../../utils/mailer");
-const multer = require("multer");
-const Controller = require("./general.controller");
+// const router = require("express").Router();
+// const mailer = require("../../utils/mailer");
+// const multer = require("multer");
+// const Controller = require("./general.controller");
 
-let Upload = multer({
-  storage: memoryStorage,
-  fileFilter(req, file, next) {
-    next(null, true);
-  },
-}).single();
+// let Upload = multer({
+//   storage: memoryStorage,
+//   fileFilter(req, file, next) {
+//     next(null, true);
+//   },
+// }).single();
 
-router.post("/sendcontactemail", async (req, res, next) => {
-  try {
-    let fields = req.body;
-    let data = await mailer.sendContactEmail(fields);
-    res.send(data);
-  } catch (error) {
-    console.log(error);
-  }
-});
+// router.post("/sendcontactemail", async (req, res, next) => {
+//   try {
+//     let fields = req.body;
+//     let data = await mailer.sendContactEmail(fields);
+//     res.send(data);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
-router.post("/changeImage", async (req, res, next) => {
-  Upload(req, res, async () => {
-    let data = await Controller.updateImage(req.files);
-    res.json(data);
-  });
-});
+// router.post("/changeImage", async (req, res, next) => {
+//   Upload(req, res, async () => {
+//     let data = await Controller.updateImage(req.files);
+//     res.json(data);
+//   });
+// });
 
-router.post("/changeText", async (req, res, next) => {
-  if (!req.body.text) {
-    res.send({
-      success: false,
-      message: "No overlay text found. Send an overlay text.",
-    });
-  }
-  let data = await Controller.updateText(req.body.text);
-  res.json(data);
-});
+// router.post("/changeText", async (req, res, next) => {
+//   if (!req.body.text) {
+//     res.send({
+//       success: false,
+//       message: "No overlay text found. Send an overlay text.",
+//     });
+//   }
+//   let data = await Controller.updateText(req.body.text);
+//   res.json(data);
+// });
 
-module.exports = router;
+// module.exports = router;
