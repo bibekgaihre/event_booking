af8fbaf32e867f23315af13f544d671d5551724e
Yarti Technologies <randomdipesh@gmail.com>
2020-07-03 07:20:47 +0545

    config removed from gitignore

---
+++ config/setup.js
@@ -0,0 +1,29 @@
+
+const { createAdmin } = require("../modules/admin/admin.controller");
+const mongoose = require("mongoose");
+const config = require("config");
+
+mongoose.connect(config.get("db.url"), {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+const register = async () => {
+  let admin = {
+    name: "test",
+    password: "test",
+    roles: ["admin"],
+    email: "test@test.com",
+  };
+  try {
+    let data = await createAdmin(admin);
+    if (data) {
+      console.log("admin created");
+      process.exit();
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+register();
